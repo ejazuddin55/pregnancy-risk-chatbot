3158dc10624701cc9ccc2b01987985dc358dc463
Ejaz Ud din <71565879+ejazuddin55@users.noreply.github.com>
2025-06-26 03:07:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -215,8 +215,8 @@ if st.session_state.current_question == len(symptom_prompts) and not st.session_
             st.session_state.responses_collected = True
             st.session_state.analyzing = False
             st.session_state.answer = answer
-            #st.session_state.risk_level = risk_level
-            #st.session_state.action = action
+            st.session_state.risk_level = risk_level
+            st.session_state.action = action
         except Exception as e:
             st.error(f"❌ An error occurred: {str(e)}. Please try again.")
             st.session_state.analyzing = False
